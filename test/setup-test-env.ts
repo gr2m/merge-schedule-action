cd455371b3a99e4827e698ee1921f85826389293
Rui Saraiva <ruisaraiva19@gmail.com>
2022-06-16 21:10:20 +0100

    feat: support only merging when all statuses are successful (#65)

--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -5,6 +5,7 @@ process.env.GITHUB_TOKEN = "some-token-here";
 process.env.GITHUB_REPOSITORY = "gr2m/merge-schedule-action";
 process.env.INPUT_MERGE_METHOD = "merge";
 process.env.INPUT_TIME_ZONE = "UTC";
+process.env.INPUT_REQUIRE_STATUSES_SUCCESS = "false";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
